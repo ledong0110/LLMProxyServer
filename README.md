b346e07bbd72b26e94bbc5c07c3a898bf1597b43
ledong0110 <74060032+ledong0110@users.noreply.github.com>
2024-07-06 23:19:15 +0700

    feat: add short tutorial

---
+++ README.md
@@ -0,0 +1,21 @@
+# Cài đặt
+```
+pip install -r requirements.txt
+```
+
+# Điều chỉnh biến môi trường
+Hãy điều chỉnh API tương ứng với LLM bạn có trong file .env
+
+# Thêm các tính năng có sẵn trong thư mục handlers/msgHandlers.py
+Xử lý logic trong thư mục này
+
+# Khởi động server
+```
+python main.py
+```
+
+# Kiểm thử
+Chạy file test.py để kiểm tra server có hoạt động không
+```
+python test.py
+```
--- test.py
+++ test.py
@@ -1,18 +1,19 @@
 import openai # openai v1.0.0+
 client = openai.OpenAI(api_key="anything",base_url="http://localhost:8080") # set proxy to base_url
 # request sent to model set on litellm proxy, `litellm --model`
-response = client.chat.completions.create(model="azure/testing", messages = [
+response = client.chat.completions.create(model="ollama_chat/hcmut_bot", messages = [
     {
         "role": "user",
-        "content": "this is a test request, write a short poem"
+        "content": "Hãy viết một bài thơ"
     }
 ],
-stream=True)
+stream=False)
 
-res = ""
-for chunk in response:
-    # print(chunk)
-    if chunk.choices[0].delta.content:
-        res += chunk.choices[0].delta.content
-        print(res)
-        print("****************")
+print(response)
+# res = ""
+# for chunk in response:
+#     # print(chunk)
+#     if chunk.choices[0].delta.content:
+#         res += chunk.choices[0].delta.content
+#         print(res)
+#         print("****************")
