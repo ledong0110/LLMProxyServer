83dad6bc95176e2150c609c3f105d13b8c72a25c
Le Dong <74060032+ledong0110@users.noreply.github.com>
2024-08-08 17:04:48 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import openai # openai v1.0.0+
 client = openai.OpenAI(api_key="anything",base_url="http://localhost:8080") # set proxy to base_url
 # request sent to model set on litellm proxy, `litellm --model`
-response = client.chat.completions.create(model="ollama_chat/hcmut_bot", messages = [
+response = client.chat.completions.create(model="ollama_chat/<your_model_name>", messages = [
     {
         "role": "user",
-        "content": "Hãy viết một bài thơ"
+        "content": "Let's write a poem"
     }
 ],
 stream=False)
